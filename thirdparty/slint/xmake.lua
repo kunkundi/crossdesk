37c4fe2e8a818f925940a8355b59fc393de5ff4f
dijunkun <junkun.di@hotmail.com>
2026-07-22 17:23:35 +0800

    build: install Rust toolchain for Slint

--- thirdparty/slint/xmake.lua
+++ thirdparty/slint/xmake.lua
@@ -10,6 +10,7 @@ package("slint")
     add_configs("shared", {description = "Build the Slint runtime as a shared library", default = true, type = "boolean", readonly = true})
 
     add_deps("cmake")
+    add_deps("rust 1.92.0", {host = true, private = true, system = false})
 
     on_load(function(package)
         package:add("includedirs", "include/slint")
