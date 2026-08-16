7e1b9cb00e02f3db2ee3d90d024ce6b4f8bf265f
dijunkun <junkun.di@hotmail.com>
2026-08-16 22:34:58 +0800

    [fix] use -ld_classic only on macOS Intel

--- xmake/platform.lua
+++ xmake/platform.lua
@@ -80,6 +80,9 @@ function setup_platform_settings()
 
         add_cxflags("-Wno-unused-variable")
     elseif is_os("macosx") then
+        if is_arch("x86_64") then
+            add_ldflags("-Wl,-ld_classic")
+        end
         add_cxflags("-Wno-unused-variable")
         add_frameworks("Cocoa", "OpenGL", "IOSurface", "ScreenCaptureKit",
             "AVFoundation", "CoreMedia", "CoreVideo", "CoreAudio",
